7db2fe9f0a7ca657d4037d38cc3ec7ff95be99cf
B-Rad-MC <mcneurb@hotmail.com>
2025-09-06 11:10:51 +1200

    Added write_file function

---
+++ functions/write_file.py
@@ -0,0 +1,14 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    try:
+        full_path = os.path.join(working_directory, file_path)
+        if not os.path.abspath(full_path).startswith(os.path.abspath(working_directory)):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if not os.path.exists(file_path):
+            os.makedirs(file_path)
+        with open(full_path, "w") as f:
+            f.write(content)
+    except Exception as e:
+        return f"Error: {e}"
+    return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
